ff3aa295335d81975ea2588de610d1fa0db759e4
BlackMagician109 <96339439+BlackMagician109@users.noreply.github.com>
2022-09-27 20:32:53 -0500

    The initial commit

---
+++ Week0/Exercise0.java
@@ -0,0 +1,83 @@
+public class Exercise0 {
+   public static void main(String[] args) {
+      String[] phoneNumbers = new String[100];
+      int[] callDurations = new int[phoneNumbers.length];
+      int size = 0;
+
+      size = addCall(phoneNumbers, callDurations, size, "555-555-5555", 137);
+      size = addCall(phoneNumbers, callDurations, size, "555-555-0000", 12);
+      size = addCall(phoneNumbers, callDurations, size, "555-555-1234", 26);
+      size = addCall(phoneNumbers, callDurations, size, "555-555-9876", 382);
+
+      System.out.println("Phone numbers (initially):");
+      printList(phoneNumbers, callDurations, size);
+
+      size = removeCall(phoneNumbers, callDurations, size, 1); // middle
+      size = removeCall(phoneNumbers, callDurations, size, size - 1); // last
+      size = removeCall(phoneNumbers, callDurations, size, 0); // first
+
+      System.out.println("\nPhone numbers (after):");
+      printList(phoneNumbers, callDurations, size);
+
+      size = removeCall(phoneNumbers, callDurations, size, 0);
+
+      System.out.println("\nPhone numbers (none left):");
+      printList(phoneNumbers, callDurations, size);
+
+      System.out.println("\nEnd of processing.");
+   }
+
+   public static int addCall(String[] phoneNumbers, int[] callDurations, int size, String newNumber, int newDuration) {
+      if (size >= phoneNumbers.length) {
+         System.out.println("Error adding " + newNumber + ": array capacity exceeded.");
+      } else {
+         phoneNumbers[size] = newNumber;
+         callDurations[size] = newDuration;
+         size++;
+      }
+
+      return size;
+   }
+
+   public static void printList(String[] phoneNumbers, int[] callDurations, int size) {
+      for (int i = 0; i < size; i++) {
+         System.out.println(phoneNumbers[i] + " duration: " + callDurations[i] + "s");
+      }
+   }
+
+   public static int find(String[] list, int size, int start, String target) {
+      int pos = start;
+
+      while (pos < size && !target.equals(list[pos])) {
+         pos++;
+      }
+
+      if (pos == size)
+         pos = -1;
+
+      return pos;
+   }
+
+   public static void findAllCalls(String[] phoneNumbers, int[] callDurations, int size, String targetNumber) {
+      int matchPos;
+
+      System.out.println("Calls from " + targetNumber + ":");
+      matchPos = find(phoneNumbers, size, 0, targetNumber);
+      while (matchPos >= 0) {
+         System.out.println(phoneNumbers[matchPos] + " duration: " + callDurations[matchPos] + "s");
+
+         // Find the next match, starting after the last one
+         matchPos = find(phoneNumbers, size, matchPos + 1, targetNumber);
+      }
+   }
+
+   public static int removeCall(String[] phoneNumbers, int[] callDurations, int size, int posToRemove) {
+      for (int i = posToRemove + 1; i < size; i++) {
+         phoneNumbers[i - 1] = phoneNumbers[i];
+         callDurations[i - 1] = callDurations[i];
+      }
+      size--;
+
+      return size;
+   }
+}
