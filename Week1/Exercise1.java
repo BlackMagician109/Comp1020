2ccda265c5635ac0349acede612e5df94ae504d4
BlackMagician109 <96339439+BlackMagician109@users.noreply.github.com>
2022-10-04 14:15:30 -0500

    Completed the changes to the code.

--- Week1/Exercise1.java
+++ Week1/Exercise1.java
@@ -1,4 +1,4 @@
-public class Activity1C {
+public class Exercise1 {
    public static void main(String[] args) {
       Item[] inventory = new Item[4];
 
@@ -58,19 +58,30 @@ public class Activity1C {
          System.out.println(inventory[i]);
       }
    }
+
+   public static void checkInventory(Item[] inventory){
+      for(int i=0 ; i<inventory.length ; i++){
+         if(inventory[i].mustReorder()){
+            order(inventory[i], 200);
+         }
+      }
+   }
 }
 
 class Item {
+   //Instance Variables
    private String name;
    private int size;
    private String code;
    private int quantity;
+   private int reorderQuantity;
 
    public Item(String name, int size, String code, int quantity) {
       this.name = name;
       this.size = size;
       this.code = code;
       this.quantity = quantity;
+      this.reorderQuantity = quantity/4;
    }
 
    public String order(int amount) {
@@ -108,7 +119,12 @@ class Item {
       } else {
          result += " quantity=0 (" + -quantity + " backordered)";
       }
+      result += " reorder quantity=" + reorderQuantity;
 
       return result;
+   }
+
+   public Boolean mustReorder(){
+      return quantity<reorderQuantity;
    }
 }
