e92830d91f9b656230d7ffa4c94e98fc15453cb6
BlackMagician109 <96339439+BlackMagician109@users.noreply.github.com>
2022-11-15 22:15:30 -0600

    Completed the program and renamed the file.

--- Week7/Exercise7.java
+++ Week7/Exercise7.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Activity7C {
+public class Exercise7 {
 	public static void main(String[] args) {
 		StudentList students = new StudentList();
 
@@ -10,6 +10,7 @@ public class Activity7C {
 		students.add(new Student(1755, "Geordi", 3.58));
 
 		students.printByNumber();
+		students.printByName();
 
 		System.out.println("\nEnd of processing.");
 	}
@@ -65,6 +66,27 @@ class StudentList {
 		}
 
 		list.add(pos + 1, student);
+	}
+
+	private void sortByName(ArrayList<Student> list){
+		Student bufferStudent;
+
+		for(int i=0 ; i<list.size()-1 ; i++){
+			if(list.get(i+1).nameComesBefore(list.get(i))){
+				bufferStudent = list.get(i);
+				list.set(i, list.get(i+1));
+				list.set(i+1, bufferStudent);
+				i = -1;
+			}
+		}
+	}
+
+	public void printByName(){
+		ArrayList<Student> toPrint = new ArrayList<Student>();
+		toPrint = (ArrayList<Student>)list.clone();
+		sortByName(toPrint);
+		System.out.println("\nStudents ordered by name:");
+		printStudents(toPrint);
 	}
 
 	public void printByNumber() {
