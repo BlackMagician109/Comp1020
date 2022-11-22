e681a3097555ac6e5e255e0f25f7a448b430a9c0
BlackMagician109 <96339439+BlackMagician109@users.noreply.github.com>
2022-11-22 17:02:39 -0600

    Added some more test cases

--- Week8/Exercise8Test.java
+++ Week8/Exercise8Test.java
@@ -1,5 +1,9 @@
 public class Exercise8Test {
     public static void main(String[] args) {
         System.out.println(Exercise8.ackermann(2, 3));
+        System.out.println(Exercise8.ackermann(3, 2));
+        System.out.println(Exercise8.ackermann(0, 3));
+        System.out.println(Exercise8.ackermann(2, 0));
+        System.out.println(Exercise8.ackermann(3, 9));
     }
 }
