e83bb9c0f7dbfd02d357c800607ede194bb2989b
BlackMagician109 <96339439+BlackMagician109@users.noreply.github.com>
2022-11-25 10:46:13 -0600

    Completed Phase1

--- Assignment2/Main.java
+++ Assignment2/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     //Main
     public static void main(String[] args) {
-        Airline airline;
+        Airline airline = new Airline();
 
         readInputFile(FILE_NAME, airline);
     }
@@ -15,9 +15,52 @@ public class Main {
     //Instance Mehtods
     public static void readInputFile(String fileName, Airline airline){
         BufferedReader inputFile;
-        String firstLine;
-        String[] data;
+        String line;
+        String[][] data = new String[99][];
+        int counter = 0;
+        try{
+            inputFile = new BufferedReader(new FileReader("textfile.txt"));
 
-        
+            line = inputFile.readLine();
+            while(line != null){
+                data[counter] = line.split(" ");
+
+                try{
+                    parseCommand(data[counter], airline);
+                }
+                catch(InvalidInputException iie){
+                    System.out.println(iie.getMessage());
+                }
+
+                try{
+                    processComment(line);
+                }
+                catch(Exception e){
+                    System.out.println(e.getMessage());
+                }
+                
+
+                counter++;
+                line = inputFile.readLine();
+            }
+
+            inputFile.close();
+        }
+        catch(IOException ioe){
+            System.out.println(ioe.getMessage());
+        }
+    }
+
+    //
+    public static void parseCommand(String[] tokens, Airline air) throws InvalidInputException{
+        if(tokens[0] == "*"){
+            
+        }
+        throw new InvalidInputException("Command not found");
+    }
+
+    //
+    public static void processComment(String data){
+        System.out.println(data.split(" ", 2)[1]);
     }
 }
